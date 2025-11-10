a58edf71eb527b02b7edc87a148aba00d3d77b60
Mubarak Ali <mubarak.ali@reading.student.ac.uk>
2025-11-10 10:03:12 +0000

    change 2

--- coursework1/AverageGrades.java
+++ coursework1/AverageGrades.java
@@ -11,20 +11,20 @@ public class AverageGrades {
             for (int j = 0; j <= numberofComponents; j++) {
                 total += grades [i][j] * weights[j];
             }
-            averages [i] = total / 100.0;
+            averages[i] = total / 100.0;
 
         }
         return averages;
     }
 
     public static void main(String[] args) {
-        int[][] grades = { { 51, 83, 28}, {0, 38, 95} }
+        int[][] grades = { { 51, 83, 28}, {0, 38, 95} };
         int[] weights = {30, 40, 30};
 
         int[] result = average_grades(grades, weights);
 
         for (int i = 0;i <= results.length; i++) {
-            System.out.println(result[i];)
+            System.out.println(result[i]);
         }
     }
 
