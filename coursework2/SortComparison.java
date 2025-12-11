28036c8663fe9a94a7913d7f3b08c83dc2153923
mubarak584569-lang <mubarak584569@gmail.com>
2025-12-11 13:29:02 +0000

    SortComparison.java

--- coursework2/SortComparison.java
+++ coursework2/SortComparison.java
@@ -33,17 +33,27 @@ public class SortComparison {
     static ArrayList<String> bubbleSort(ArrayList<String> array) {
          ArrayList<String> arr = new ArrayList<>(array);
 
+         
          boolean swapped;
-         for (int i = 0; i < arr.size( - 1; j++)) {
-            if (cardCompare(arr.get(j), arr.get (j + 1)) > 0){
-                String temp = arr.get(j);
-                arr.set(j, arr.get(j +1));
-                arr.set(j + 1, temp);
-                swapped = true;
-            }
+         
+         for (int i = 0; i < arr.size() - 1; i++) {
+            swapped = false;
+        
+            for (int j = 0; j < arr.size() - 1 - i; j++) { 
+                if (cardCompare(arr.get(j), arr.get (j + 1)) > 0) { 
+
+                    String temp = arr.get(j); 
+                    arr.set(j, arr.get(j + 1));
+                    arr.set(j + 1, temp);
+                    swapped = true;              
+                }
          }
         
-         if (!swapped) break; 
-    }
+         if (!swapped) {
+            break;
 
+         }
+    }
+    return arr;
+    
 }
